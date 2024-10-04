58f5ed4229c4eb64cb5a71dc4f7f583972ce083d
Paul <Paul@yandex.ru>
2024-10-05 02:04:21 +0300

    10-ый раз исправил логику и запись в файл в сервисе для упаковки

--- src/main/java/ru/liga/consoleParcels/service/impl/OptimizedPackagingService.java
+++ src/main/java/ru/liga/consoleParcels/service/impl/OptimizedPackagingService.java
@@ -2,7 +2,9 @@ package ru.liga.consoleParcels.service.impl;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import ru.liga.consoleParcels.dto.ParcelCountDto;
 import ru.liga.consoleParcels.dto.ParcelForPackagingDto;
+import ru.liga.consoleParcels.dto.TruckParcelCountDto;
 import ru.liga.consoleParcels.exception.PackingException;
 import ru.liga.consoleParcels.factory.TruckFactory;
 import ru.liga.consoleParcels.model.Point;
@@ -12,8 +14,10 @@ import ru.liga.consoleParcels.service.ParcelCountingService;
 import ru.liga.consoleParcels.service.ParcelQuantityRecordingService;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Реализация сервиса оптимизированной упаковки.
@@ -44,18 +48,17 @@ public class OptimizedPackagingService implements PackagingService {
 
     @Override
     public List<Truck> packPackages(List<ParcelForPackagingDto> parcels, String trucksSize) {
-//        parcels.sort((p1, p2) -> Integer.compare(p2.getArea(), p1.getArea()));
-//        log.trace("Посылки отсортированы по площади в порядке убывания");
-//
-//        List<Truck> trucks = loadTrucks(parcels, trucksSize);
-//
-//        Map<String, Integer> parcelCountByShape = parcelCountingService.countParcelByShape(parcels);
-//
-//        parcelQuantityRecordingService.writeParcelCountToJsonFile(parcelCountByShape);
-//
-//        log.info("Упаковка завершена успешно. Все посылки размещены.");
-//        return trucks;
-        return new ArrayList<>();
+        parcels.sort((p1, p2) -> Integer.compare(p2.getArea(), p1.getArea()));
+        log.trace("Посылки отсортированы по площади в порядке убывания");
+
+        List<Truck> trucks = loadTrucks(parcels, trucksSize);
+
+        List<TruckParcelCountDto> truckParcelCounts = countParcelsInTrucks(trucks);
+
+        parcelQuantityRecordingService.writeParcelCountToJsonFile(truckParcelCounts);
+
+        log.info("Упаковка завершена успешно. Все посылки размещены.");
+        return trucks;
     }
 
     private List<Truck> loadTrucks(List<ParcelForPackagingDto> parcels, String trucksSize) {
@@ -75,5 +78,31 @@ public class OptimizedPackagingService implements PackagingService {
                         }));
 
         return trucks;
+    }
+
+    private List<TruckParcelCountDto> countParcelsInTrucks(List<Truck> trucks) {
+        List<TruckParcelCountDto> truckParcelCounts = new ArrayList<>();
+
+        for (int i = 0; i < trucks.size(); i++) {
+            Truck truck = trucks.get(i);
+            Map<String, Integer> parcelCountByShape = new HashMap<>();
+
+            for (char[] row : truck.getSpace()) {
+                for (char cell : row) {
+                    if (cell != ' ') {
+                        String shape = String.valueOf(cell);
+                        parcelCountByShape.put(shape, parcelCountByShape.getOrDefault(shape, 0) + 1);
+                    }
+                }
+            }
+
+            List<ParcelCountDto> parcelCounts = parcelCountByShape.entrySet().stream()
+                    .map(entry -> new ParcelCountDto(entry.getKey(), entry.getValue()))
+                    .collect(Collectors.toList());
+
+            truckParcelCounts.add(new TruckParcelCountDto(i + 1, parcelCounts));
+        }
+
+        return truckParcelCounts;
     }
 }
